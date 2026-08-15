2671ce0b52da4b9c53b14b93a1d59279340380a4
sbu001monterecco <sbu001@monterecco.com>
2026-08-15 19:51:16 +0100

    Fix Canarias7 article preview visibility (#73)
    * Add Canarias7 preserved article preview injection
    
    * Load Canarias7 article preview fix

--- assets/site.js
+++ assets/site.js
@@ -17,7 +17,9 @@
           load('institutional-accountability-spotlights-20260815.js?v=20260815a', () => {
             load('institutional-accountability-backlog-20260815.js?v=20260815a', () => {
               load('media-accountability-navigation-20260815.js?v=20260815a', () => {
-                load('media-followup-pressrelease-fiscal-20260815.js?v=20260815a');
+                load('media-followup-pressrelease-fiscal-20260815.js?v=20260815a', () => {
+                  load('canarias7-preview-fix-20260815.js?v=20260815a');
+                });
               });
             });
           });
