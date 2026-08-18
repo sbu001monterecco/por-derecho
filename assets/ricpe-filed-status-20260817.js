cd3224bb7c0a214bdf5af20df5e4dbfa13b01638
sbu001monterecco <sbu001@monterecco.com>
2026-08-18 10:34:05 +0100

    Load optimum reader journey after existing presentation layers

--- assets/ricpe-filed-status-20260817.js
+++ assets/ricpe-filed-status-20260817.js
@@ -43,4 +43,5 @@
   load('reader-journey-hero-20260818.js', 'data-psr-reader-journey-hero', '20260818b');
   load('practitioner-open-kimono-20260818.js', 'data-open-kimono-practitioner', '20260818b');
   load('supervisory-practice-entrypoints-20260818.js', 'data-supervisory-practice-entrypoints', '20260818a');
+  load('optimum-reader-journey-20260818.js', 'data-optimum-reader-journey', '20260818a');
 })();
