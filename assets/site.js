4e6d9e243605ce4fedc008d990c4f8d8fdf51063
sbu001monterecco <sbu001@monterecco.com>
2026-09-04 14:14:18 +0100

    Load Asier Barañano CaixaBank expert actor module

--- assets/site.js
+++ assets/site.js
@@ -26,6 +26,7 @@
     load('unitary-criminal-source-register-search-extension-20260904.js', 'data-unitary-criminal-source-register-search-loader', '20260904a');
     load('home-mission-critical-20260904.js', 'data-home-mission-critical-loader', '20260904a');
     load('caixabank-valencia-concurso-cam-linkage-20260904.js', 'data-caixabank-valencia-concurso-cam-linkage-loader', '20260904a');
+    load('caixabank-asier-baranano-expert-actor-20260904.js', 'data-caixabank-asier-baranano-expert-actor-loader', '20260904a');
     load('caixabank-valencia-lawyer-dataroom-unitary-20260904.js', 'data-caixabank-valencia-lawyer-dataroom-unitary-loader', '20260904a');
     load('caixabank-valencia-lawyer-dataroom-deeplinks-20260904.js', 'data-caixabank-valencia-lawyer-dataroom-deeplinks-loader', '20260904a');
     load('caixabank-valencia-unitary-inbound-interlinks-20260904.js', 'data-caixabank-valencia-unitary-inbound-interlinks-loader', '20260904a');
