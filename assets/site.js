121f97bfd8071b6abe8e2454c07c4d4bb9f419b1
sbu001monterecco <sbu001@monterecco.com>
2026-09-16 13:10:09 +0100

    Sun Rock: load capital relationship routing

--- assets/site.js
+++ assets/site.js
@@ -40,6 +40,7 @@
     load('joan-cruz-multitrack-crosslinks-20260901.js', 'data-joan-cruz-multitrack-loader', '20260904a');
     load('orion-rental-socimi-interlinks-20260905.js', 'data-orion-rental-socimi-interlinks-loader', '20260905a');
     load('orion-rental-socimi-search-extension-20260905.js', 'data-orion-rental-socimi-search-loader', '20260905a');
+    load('capital-relationships-route-20260916.js', 'data-capital-relationships-route-loader', '20260916a');
   };
 
   const loadHotelFincaSystem = () => {
